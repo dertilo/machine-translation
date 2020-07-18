4bb958b64c9198b73c746104751e616bb273bf08
Tilo Himmelsbach <dertilo@googlemail.com>
2020-07-18 12:46:03 +0200

    debug_distillation.py

--- distillation.py
+++ distillation.py
@@ -397,34 +397,7 @@ def distill_main(args):
 
 
 if __name__ == "__main__":
-    debug_args = """
---data_dir=some_data \
---src_lang=en_XX \
---tgt_lang=ro_RO \
---model_name_or_path IGNORED \
---learning_rate=3e-4 \
---train_batch_size=4 \
---eval_batch_size=4 \
---teacher sshleifer/tiny-mbart \
---tokenizer_name sshleifer/tiny-mbart \
---warmup_steps 500 \
---student_decoder_layers 2 --student_encoder_layers 2 \
---freeze_encoder --freeze_embeds \
---alpha_hid=3. --length_penalty=0.5 \
---gradient_accumulation_steps=2
---max_target_length=60 --val_max_target_length=60 --test_max_target_length=100 \
---output_dir=debug \
---num_train_epochs 10 \
---gpus 0 \
---do_train \
---do_predict \
---n_val 1000 \
---val_check_interval 0.1 \
---sortish_sampler \
-    """.strip().split()
-    # --fp16 \
     parser = argparse.ArgumentParser()
     parser = BartTranslationDistiller.add_model_specific_args(parser, os.getcwd())
-    args = parser.parse_args(debug_args)
-
+    args = parser.parse_args()
     distill_main(args)
